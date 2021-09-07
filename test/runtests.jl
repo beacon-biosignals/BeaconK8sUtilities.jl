5d9b95b6dc67f1ab2301c0af508068ca70e76722
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-09-07 16:01:54 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
 @testset "K8sUtilities.jl" begin
     @testset "setup_tensorboard" begin
         testdir = mktempdir()
-        test_args = (; app="test-app", logdir="test-logdir", ecr="test-ecr", service_account="test-service-account")
+        test_args = (; app="test-app", logdir="test-logdir", ecr="test-ecr", service_account="test-service-account", namespace="test-namespace")
         @test setup_tensorboard(testdir; test_args...) === nothing
 
         # Check we at least get the right files
