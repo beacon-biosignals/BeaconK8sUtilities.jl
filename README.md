9121889b0cc58cffa548b3556626a2df77b8a44f
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-09-16 14:54:45 +0200

    Add `follow` utility (#2)
    * wip
    
    * format, cleanup
    
    * restore `wait` behavior
    
    * add example
    
    * update readme
    
    * make error more informative

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://beacon-biosignals.github.io/K8sUtilities.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://beacon-biosignals.github.io/K8sUtilities.jl/dev)
 [![Build Status](https://github.com/beacon-biosignals/K8sUtilities.jl/workflows/CI/badge.svg)](https://github.com/beacon-biosignals/K8sUtilities.jl/actions)
+
+
+## Example
+
+We can easily setup a simple script to follow as a pod launches:
+```julia
+julia> using K8sUtilities
+
+julia> setup_follow("scripts")
+
+```
+
+Then we can run `./scripts/follow.sh pod_name` from our shell and get
+a progress bar as we wait for the pod to startup, and then the logs streamed
+once it is ready. (This is just like `kubectl logs -f pod_name` except that
+we don't have to wait for the pod to be ready first).
+
+This could be adapted into a whole "launch script" including building a docker image,
+pushing it to an ECR, launching a pod, and then following along.
+Such a script can be facilitated by the Julia-side utilities and preferences
+provided in K8sUtilites.jl; see the [documentation](https://beacon-biosignals.github.io/K8sUtilities.jl/dev) for more.
--- src/templates.jl
+++ src/templates.jl
@@ -11,6 +11,37 @@ function gen_file(file, text::AbstractString)
     return write(file, text)
 end
 
+function make_executable(path)
+    try
+        # Uses FilePathsBase so we can do "+x"
+        chmod(Path(path), "+x")
+    catch e
+        @error "Could not run `chmod +x $path`. Try running this manually." exception = e
+    end
+    return nothing
+end
+
+function render_dir(template_dir, destination, variables; overwrite)
+    for name in readdir(template_dir)
+        text = render(read(joinpath(template_dir, name), String), variables)
+        path = joinpath(destination, name)
+        if !overwrite && isfile(path)
+            error("$path already exists; set `overwrite=true` to overwrite existing files.")
+        end
+        @info("Writing $(path)")
+        gen_file(path, text)
+    end
+    return nothing
+end
+
+function check_not_file(destination)
+    isfile(destination) && throw(ArgumentError("""
+            Destination $destination exists and is a file.
+            Must be a directory (or nonexistent in which case a directory will be created).
+        """))
+    return nothing
+end
+
 """
     setup_tensorboard(destination::AbstractString; app::AbstractString,
                       logdir::AbstractString,
@@ -40,42 +71,59 @@ function setup_tensorboard(destination::AbstractString; app::AbstractString,
                            service_account::AbstractString=default_service_account(),
                            namespace::AbstractString=get_current_namespace(),
                            local_port::Int=6006, overwrite=false)
-    isfile(destination) && throw(ArgumentError("""
-            Destination $destination exists and is a file.
-            Must be a directory (or nonexistent in which case a directory will be created).
-        """))
-
+    check_not_file(destination)
     variables = Dict("app" => app, "logdir" => logdir, "ecr" => ecr,
                      "service_account" => service_account, "local_port" => local_port,
                      "namespace" => namespace)
-
-    template_dir = joinpath(TEMPLATES, "tensorboard")
-
-    for name in readdir(template_dir)
-        text = render(read(joinpath(template_dir, name), String), variables)
-        path = joinpath(destination, name)
-        if !overwrite && isfile(path)
-            error("$path already exists; set `overwrite=true` to overwrite existing files.")
-        end
-        @info("Writing $(path)")
-        gen_file(path, text)
-    end
-
-    @info tensorboard_instructions()
-
+    render_dir(joinpath(TEMPLATES, "tensorboard"), destination, variables; overwrite)
+    make_executable(joinpath(destination, "tensorboard.sh"))
+    @info tensorboard_instructions(destination)
     return nothing
 end
 
-function tensorboard_instructions()
+function tensorboard_instructions(destination)
     return """
-After calling `setup_tensorboard(destination; kwargs...)` to setup the configuration scripts in a destination directory:
+Tensorboard has been configured in $destination.
 
-1. Run `chmod +x destination/tensorboard.sh` to make the script executable.
-2. Make sure to add `K8sUtilities` to your global Julia environment so that it can be used from the `tensorboard.sh` script.
+Make sure to add `K8sUtilities` to your global Julia environment so that it can be used from the `tensorboard.sh` script.
 
-Then running `destination/tensorboard.sh` in a shell should launch a tensorboard pod,
+Then running `$destination/tensorboard.sh` in a shell should launch a tensorboard pod,
 or give you the option to connect to an existing one.
 
 Note: You can edit these files freely; running `setup_tensorboard` with `overwrite=true` will replace them with the latest defaults.
+"""
+end
+
+"""
+    setup_follow(destination::AbstractString;
+                 namespace::AbstractString=get_current_namespace(), overwrite=false)
+
+Sets up a `follow.sh` script in destination directory `destination`, which
+does not have to exist ahead of time.
+
+* `namespace`: name of the Kubernetes namespace to use. Defaults to the current active
+  one introspected by [`get_current_namespace`](@ref).
+* `overwrite`: whether or not to overwrite existing configuration scripts.
+
+"""
+function setup_follow(destination::AbstractString;
+                      namespace::AbstractString=get_current_namespace(), overwrite=false)
+    check_not_file(destination)
+    variables = Dict("namespace" => namespace)
+    render_dir(joinpath(TEMPLATES, "follow"), destination, variables; overwrite)
+    make_executable(joinpath(destination, "follow.sh"))
+    @info follow_instructions(destination)
+    return nothing
+end
+
+function follow_instructions(destination)
+    return """
+`follow.sh` has been configured in $destination.
+
+Make sure to add `K8sUtilities` to your global Julia environment so that it can be used from the `follow.sh` script.
+
+Then running `$destination/follow.sh pod_name` should let you follow the progress of a pod.
+
+Note: You can edit these files freely; running `setup_follow` with `overwrite=true` will replace them with the latest defaults.
 """
 end
