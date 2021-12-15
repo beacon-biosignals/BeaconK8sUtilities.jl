de19202b266c42b0503f408791411d81894639d2
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-12-14 23:36:49 -0500

    update docstrings

--- src/logger.jl
+++ src/logger.jl
@@ -6,8 +6,22 @@ end
 """
     jsonable(obj::Any) = obj
 
-Add methods to `BeaconK8sUtilities.jsonable` in order to specify how `json_logger` should
+Add methods to `BeaconK8sUtilities.jsonable` in order to specify how [`json_logger`](@ref) should
 serialize types that themselves do not already have a `StructTypes.StructType` defined.
+
+## Example
+
+For example, if we want to log `TimerOutput`s from TimerOutputs.jl, we can define the
+following.
+
+```julia
+using TimerOutputs
+
+BeaconK8sUtilities.jsonable(to::TimerOutput) = TimerOutputs.todict(to)
+```
+
+This pirates `BeaconK8sUtilities.jsonable` but since it is only used for the logger,
+it is better than pirating `StructTypes.StructType` which could be used more widely.
 """
 jsonable(obj::Any) = obj
 
@@ -33,9 +47,11 @@ Provides a logger which:
 * Emits logs as JSON (via `LoggingFormats.JSON(; recursive=true)`)
 * Adds extra information to every log message via `info_for_logger()`, which can be any function that evaluates
 to a `NamedTuple`. The default provides a timestamp and the current worker id (`Distributed.myid()`).
-* transforms `exception` keys to render backtraces to strings before logging them
+* the respects `maxlog` convention (see <https://docs.julialang.org/en/v1/stdlib/Logging/#Logging.@logmsg>)
 
 Set the positional argument to the minimum-enabled logging level, and `io` to the IO handle where the logs should be emitted.
+
+See also [`BeaconK8sUtilities.jsonable`](@ref) for how to customize how objects are converted to JSON.
 """
 function json_logger(level=Logging.Info; info_for_logger=info_for_logger, io=stderr)
     t = TransformerLogger(FormatLogger(LoggingFormats.JSON(; recursive=true), io)) do log
